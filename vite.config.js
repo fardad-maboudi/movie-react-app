c4e932ba39bc3667281a418c368e144b6386a948
fardad-maboudi <fardad.mg75@yahoo.com>
2025-12-11 14:05:06 +0330

    fix 3 refresh issue

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
+//process.env.VITE_BASE_PATH || '/movie-react-app',
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.VITE_BASE_PATH || '/movie-react-app',
+  base: process.env.NODE_ENV === 'production' ? '/' : '/',
   plugins: [
     react()
   ],
